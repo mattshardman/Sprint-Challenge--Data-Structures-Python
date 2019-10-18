26e2add90b204519bbf1bdf363c1db9130b531be
mattshardman <mattshardman@gmail.com>
2019-10-18 09:49:38 +0100

    refactor

--- names/names.py
+++ names/names.py
@@ -48,33 +48,56 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-# names_2_dict = {}
-
-# for name in names_2:
-#     if not names_2_dict.get(name[0]):
-#         names_2_dict[name[0]] = BinarySearchTree(name)
-#     else:
-#        names_2_dict[name[0]].insert(name) 
-
-# duplicates = []
-
-# for name_1 in names_1:
-#     if names_2_dict[name_1[0]]:
-#         contains = names_2_dict[name_1[0]].contains(name_1)
-#         if contains:
-#             duplicates.append(name_1)
-
-
-names_2_dict = {}
-
-for name in names_2:
-    names_2_dict[name] = True
-
 duplicates = []
 
-for name_1 in names_1:
-    if names_2_dict.get(name_1):
-        duplicates.append(name_1)
+def bin_search_names():
+    names_2_tree = BinarySearchTree(names_2[0])
+
+    for name in names_2[1:]:
+        names_2_tree.insert(name)
+
+    for name_1 in names_1:
+        contains = names_2_tree.contains(name_1)
+        if contains:
+            duplicates.append(name_1)
+
+
+def bin_search_dict_names():
+    names_2_dict = {}
+
+    for name in names_2:
+        if not names_2_dict.get(name[0]):
+            names_2_dict[name[0]] = BinarySearchTree(name)
+        else:
+            names_2_dict[name[0]].insert(name) 
+
+    for name_1 in names_1:
+        if names_2_dict[name_1[0]]:
+            contains = names_2_dict[name_1[0]].contains(name_1)
+            if contains:
+                duplicates.append(name_1)
+
+def dic_search_names():
+    names_2_dict = {}
+
+    for name in names_2:
+        names_2_dict[name] = True
+
+    for name_1 in names_1:
+        if names_2_dict.get(name_1):
+            duplicates.append(name_1)
+
+def embedded_comp_search():
+    duplicates = []
+
+    for name_1 in names_1:
+        if name_1 in names_2:
+            duplicates.append(name_1)
+
+# bin_search_names()
+# bin_search_dict_names()
+dic_search_names() # quickest
+# embedded_comp_search()
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
